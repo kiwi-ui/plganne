57c2e8e092c40ed5e3343c7c3bf2fd6b42e9dc1e
yusni anggara <yusnianggara@gmail.com>
2025-08-31 20:30:30 +0700

    hai

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
