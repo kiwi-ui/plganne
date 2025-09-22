46d45aa287939959e14a852c9a38d45e6cdea73b
yusni anggara <yusnianggara@gmail.com>
2025-09-22 21:14:54 +0700

    fix date

--- src/components/Reminder/index.jsx
+++ src/components/Reminder/index.jsx
@@ -3,9 +3,9 @@ import { AddToCalendarButton } from 'add-to-calendar-button-react';
 const Reminder = () => {
   return (
     <AddToCalendarButton
-        name="Sisin & Tiar Wedding (Resepsi)"
+        name="Sisin & Ikhtiar Wedding (Resepsi)"
         location="Masjid Agung Asy-Syuhada, Jl. Mesigit No.23, Gladak Anyar, Barurambat Kota, Kec. Pamekasan, Kabupaten Pamekasan, Jawa Timur 69317"
-        startDate="2025-11-19"
+        startDate="2025-10-19"
         useUserTZ="true"
         size='2'
         options="['Apple','Google']"
